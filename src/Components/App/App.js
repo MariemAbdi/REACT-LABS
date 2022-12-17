7f1841e705a13785afc31aae2be77691f4aed5f5
MariemAbdi <mariem.abdi2000@gmail.com>
2022-12-17 22:11:12 +0100

    working

---
+++ src/Components/App/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login';
+import useToken from './useToken';
+
+function App() {
+
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  if(token){
+    return <Dashboard />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
